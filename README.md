c95a63f334a0d16ef425c5a02930c351c6889ab2
wuwenyi <wwwwwydev@gmail.com>
2024-11-21 15:48:42 +0800

    up description

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# cache
-a cache framework for golang
+<!-- markdownlint-disable MD033 MD041 -->
+<div align="center">
+
+# 🪡cache
+
+<!-- prettier-ignore-start -->
+<!-- markdownlint-disable-next-line MD036 -->
+a simple cache framework for golang
+<!-- prettier-ignore-end -->
+
+<img src="https://img.shields.io/badge/golang-1.21+-blue" alt="golang">
+</div>
+
+## installing
+Select the version to install
+
+`go get github.com/go-needle/cache@version`
+
+If you have already get , you may need to update to the latest version
+
+`go get -u github.com/go-needle/cache`
+
+
+## quickly start
+```golang
+package main
+
+import (
+	"fmt"
+	"github.com/go-needle/cache"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+func main() {
+	c := cache.NewLRU(1<<32, time.Duration(10)*time.Second)
+	// c := cache.NewFIFO(1<<32, time.Duration(10)*time.Second)
+	for i := 0; i < 1000; i++ {
+		c.Add("key"+strconv.Itoa(i), []byte(strconv.Itoa(i)))
+	}
+	for i := 0; i < 1000; i++ {
+		num := i
+		go func() {
+			time.Sleep(time.Duration(rand.Intn(20)) * time.Second)
+			v, ok := c.Get("key" + strconv.Itoa(num))
+			fmt.Println(v, ok)
+		}()
+	}
+	time.Sleep(time.Duration(20) * time.Second)
+}
+```
--- cache.go
+++ cache.go
@@ -18,10 +18,12 @@ type LRUCache struct {
 	maxSurvivalTime time.Duration
 }
 
+// NewLRU creates a new cache struct and use the LRU algorithm
 func NewLRU(cacheBytes int64, maxSurvivalTime time.Duration) *LRUCache {
 	return &LRUCache{maxSurvivalTime: maxSurvivalTime, cacheBytes: cacheBytes}
 }
 
+// Add is safe for concurrent access.
 func (c *LRUCache) Add(key string, value []byte) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -37,6 +39,7 @@ func (c *LRUCache) Add(key string, value []byte) {
 	}()
 }
 
+// Get is safe for concurrent access.
 func (c *LRUCache) Get(key string) ([]byte, bool) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -56,10 +59,12 @@ type FIFOCache struct {
 	maxSurvivalTime time.Duration
 }
 
+// NewFIFO creates a new cache struct and use the FIFO algorithm
 func NewFIFO(cacheBytes int64, maxSurvivalTime time.Duration) *LRUCache {
 	return &LRUCache{maxSurvivalTime: maxSurvivalTime, cacheBytes: cacheBytes}
 }
 
+// Add is safe for concurrent access.
 func (c *FIFOCache) Add(key string, value []byte) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -75,6 +80,7 @@ func (c *FIFOCache) Add(key string, value []byte) {
 	}()
 }
 
+// Get is safe for concurrent access.
 func (c *FIFOCache) Get(key string) ([]byte, bool) {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
